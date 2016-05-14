f0f455b551b9f03f5ba93b3506c6589a4b575a63
Jack Barrett <jackbarrett@linux.com>
2016-05-14 17:22:56 -0400

    added a simple test

--- index.js
+++ index.js
@@ -1,7 +1,2 @@
 var iopi = require("./build/Release/iopi.node");
-iopi.init(0x21);
-iopi.setPinDirection(0x21, 1, 0x00);
-iopi.writePin(0x21, 1, 1);
-setTimeout(() => {
-  iopi.writePin(0x21, 1, 0);
-}, 5000);
+module.exports=iopi;
---
+++ tests/led.js
@@ -0,0 +1,46 @@
+var iopi = require("../index");
+var sleep = require("sleep").sleep;
+var usleep = require("sleep").usleep;
+var bus=0x27;
+iopi.init(bus);
+
+var led={
+  channels:{
+    red:1,
+    green:2,
+    blue:3
+  },
+  openChannel:(channel)=>{
+    iopi.setPinDirection(bus,this.channels[channel],0);
+    iopi.writePin(bus, this.channels[channel], 1);
+  },
+  closeChannel:(channel)=>{
+    iopi.setPinDirection(bus,this.channels[channel],0);
+    iopi.writePin(bus, this.channels[channel], 1);
+  },
+};
+process.on('SIGINT', ()=>{
+  led.closeChannel("red");
+  led.closeChannel("green");
+  led.closeChannel("blue");
+}.bind(null, {exit:true}));
+
+while(true){
+  led.openChannel("red");
+  led.closeChannel("red");
+  sleep(3);
+  led.openChannel("green");
+  led.closeChannel("green");
+  sleep(3);
+  led.openChannel("blue");
+  led.closeChannel("blue");
+  sleep(3);
+}
+// iopi.setPinDirection(0x27, 1, 0x00);
+// iopi.writePin(0x27, 1, 1);
+// iopi.setPinDirection(0x27, 2, 0x00);
+// iopi.writePin(0x27, 2, 1);
+// setTimeout(() => {
+//   iopi.writePin(0x27, 1, 0);
+//   iopi.writePin(0x27, 2, 0);
+// }, 500);
