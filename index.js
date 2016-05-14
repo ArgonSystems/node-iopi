9015ce5227e6ea18534c778bb356195c736cd9b9
Charles Barrette <cfbarrette@gmail.com>
2016-05-14 01:57:22 -0400

    Modified index

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 var iopi = require("./build/Release/iopi.node");
-console.log(iopi.init(0x10));
+iopi.init(0x21);
+iopi.setPinDirection(0x21, 1, 0x00);
+iopi.writePin(0x21, 1, 1);
+setTimeout(() => {
+  iopi.writePin(0x21, 1, 0);
+}, 5000);
