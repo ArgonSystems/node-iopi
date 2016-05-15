1b80f9eef4f6ff554560b891efe832402f03d382
Jack Barrett <jackbarrett@linux.com>
2016-05-14 21:52:17 -0400

    more buttons

--- tests/button-alternate.js
+++ tests/button-alternate.js
@@ -1,14 +1,22 @@
 var LED = require("./led");
 var Button = require("./button");
 var leds=[
-  new LED(0x20,{red:1}),
-  new LED(0x20,{red:2}),
-  new LED(0x20,{red:3}),
-  new LED(0x20,{red:4}),
-  new LED(0x20,{red:5}),
-  new LED(0x20,{red:6}),
-  new LED(0x20,{red:7}),
-  new LED(0x20,{red:8}),
+  new LED(0x27,{red:1}),
+  new LED(0x27,{red:2}),
+  new LED(0x27,{red:3}),
+  new LED(0x27,{red:4}),
+  new LED(0x27,{red:5}),
+  new LED(0x27,{red:6}),
+  new LED(0x27,{red:7}),
+  new LED(0x27,{red:8}),
+  new LED(0x27,{red:9}),
+  new LED(0x27,{red:10}),
+  new LED(0x27,{red:11}),
+  new LED(0x27,{red:12}),
+  new LED(0x27,{red:13}),
+  new LED(0x27,{red:14}),
+  new LED(0x27,{red:15}),
+  new LED(0x27,{red:16}),
 ];
 process.on('SIGINT', function(){
   for(let x in leds){
